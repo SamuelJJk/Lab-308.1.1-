22a0125c1dfc1efdb955dd6790a09300ecf61426
SamuelJJk <samueljean.j24@gmail.com>
2024-03-13 14:47:15 -0400

    finshed problem 1 and 2

--- app.js
+++ app.js
@@ -49,13 +49,72 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   //Math problem 1//
   //////////////////
 
-  function dividisbleBy_5(a) {
-    if (a % 5 === 0){
-        console.log("Yes");
-    }
-    else{
-        console.log("No")
-    }
-  }
 
-  dividisbleBy_5()
+//   Check if all numbers are divisible by 5. 
+let dividBy5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) === 0 
+console.log(dividBy5)
+
+
+// Check if the first number is larger than the last.
+let greaterThan = n1 > n4;
+console.log(greaterThan)
+
+
+// Subtract the first number from the second number.
+let subtract = n1 - n2;
+console.log(subtract);
+// Multiply the result by the third number
+let mulitply = subtract * n3;
+console.log(mulitply);
+// Find the remainder of dividing the result by the fourth number.
+let remainder = mulitply % n4;
+console.log(remainder);
+
+
+// Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+
+// const isOver25 = ((n1 > 25) || (n2 > 25) || (n3 > 25) || (n4 > 25));
+
+console.log(isOver25);
+
+
+// part 2 practical math
+let mph_55 = 55;
+let mph_60 = 60;
+let mph_75 = 75;
+let totalMiles = 1500;
+let budget = 175;
+let avgCost = 3;
+
+ // 55
+    let totalMilesFor55 = totalMiles / mph_55;
+    console.log("This is total miles for 55, "+ totalMilesFor55); 
+
+    let totalGallonFor55 = totalMilesFor55 * 30 ;
+    console.log("This is total gallons for 55, "+ totalGallonFor55);
+
+    let costFor55 = totalGallonFor55 / avgCost;
+    console.log("This is total cost for 55, "+ costFor55);
+
+
+ //60 
+ let totalMilesFor60 = totalMiles / mph_60;
+ console.log("This is total miles for 60, "+ totalMilesFor60); 
+
+ let totalGallonFor60 = totalMilesFor60 * 28 ;
+ console.log("This is total gallons for 60, "+ totalGallonFor60);
+
+ let costFor60 = totalGallonFor60 / avgCost;
+ console.log("This is total cost for 60, "+ costFor60);
+
+
+
+ //75
+ let totalMilesFor75 = totalMiles / mph_75;
+ console.log("This is total miles for 75, "+ totalMilesFor55); 
+
+ let totalGallonFor75 = totalMilesFor75 *  23;
+ console.log("This is total gallons for 75, "+ totalGallonFor75);
+
+ let costFor75 = totalGallonFor75 / avgCost;
+ console.log("This is total cost for 75, "+ costFor75);
